a084d10198eef78e89339b7c9be2e98f4e649248
MD. Mahmudul Hasan <61178414+mahmudulri@users.noreply.github.com>
2022-10-12 09:30:22 +0600

    nothing

--- lib/main.dart
+++ lib/main.dart
@@ -32,6 +32,6 @@ class MyApp extends StatelessWidget {
         theme: ThemeData(
           primarySwatch: Colors.blue,
         ),
-        home: DraftPage());
+        home: MessageBox());
   }
 }
